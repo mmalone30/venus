60da0a0d5a5094640a4dcfec37cdca390cde6240
Mitch Malone <mmalone@WM-MMALONE-MBP.local>
2018-05-13 14:17:23 -0700

    trying some new typefaces

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -5,8 +5,10 @@ $(window).scroll(function() {
   var windowScroll = $(window).scrollTop();
   if (windowScroll > 96) {
     $("header.article").addClass("scrolling");
+    $("header.article h3").addClass("show");
   } else {
     $("header.article").removeClass("scrolling");
+    $("header.article h3").removeClass("show");
   }
 
  console.log(windowScroll);
